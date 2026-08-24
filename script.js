baab3a4a9c92b4da101af62073f6cb167438f700
stefaniqsantos-coder <stefani.q.santos@ba.estudante.senai.br>
2026-08-24 16:31:41 -0300

    stefani

---
+++ script.js
@@ -0,0 +1,33 @@
+//Dica: normalmente colocamos os elementos capturados dentro de ums constante
+const html = document.querySelector('html');
+//BUTTONS
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const botaoIniciar = document.querySelector('.app__card-primary-button')
+
+//HEADER
+const titulo = document.querySelector('.app__title')
+const banner = document.querySelector('.app__image')
+
+//TIMER
+const displayTempo = document.querySelector('#timer')
+
+//CRIAR DOS OUVINTES DE EVENTOS
+focoBt.addEventListener('click',() => {
+    html.setAttribute('data-contexto','foco')
+    banner.setAttribute('src', '/imagens/foco.png')
+})
+
+curtoBt.addEventlistener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto','descanso-longo' )
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
+})
+
+
+
