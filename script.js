93279be1340e8c1ba1880bc2987e76ec145ab6d7
stefaniqsantos-coder <stefani.q.santos@ba.estudante.senai.br>
2026-08-25 16:27:37 -0300

    stefani

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-//Dica: normalmente colocamos os elementos capturados dentro de ums constante
+//DICA: Normalmente colocamos os elementos capturados dentro de uma constante
 const html = document.querySelector('html');
+
 //BUTTONS
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
+const  focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
 const botaoIniciar = document.querySelector('.app__card-primary-button')
+const botoes = document.querySelectorAll('.app__card-button');
 
 //HEADER
-const titulo = document.querySelector('.app__title')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 //TIMER
-const displayTempo = document.querySelector('#timer')
+const displayTimer = document.querySelector('#timer')
 
-//CRIAR DOS OUVINTES DE EVENTOS
-focoBt.addEventListener('click',() => {
-    html.setAttribute('data-contexto','foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco');
+    focoBt.classList.add('active')
 })
 
-curtoBt.addEventlistener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto','descanso-longo' )
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active')
 })
 
+function alterarContexto(contexto){
+    
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+})
 
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+
+    switch (contexto){
+        case "foco":
+        titulo.innerHTML =
+        `
+        Otimize sua produtividade,<br>
+        <strong class="app__title-strong">Mergulhe no que importa.</strong>
+        `
+        break;
+    
+    case "descanso-curto":
+        titulo.innerHTML =
+        `
+        Que tal dar uma respirada? <br>
+        <strong class="app__title-strong">Faça uma pausa curta?</strong>
+        `
+        break;
+    
+        case "descanso-longo":
+        titulo.innerHTML =
+        `
+        Hora de voltar á superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>
+        `
+        break;
+    
+    }
+    
+}
 
